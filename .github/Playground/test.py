8c2677e6d76d092829aab2fe6d9062a2a778ce84
JaymeSileo <129315115+JaymeSileo@users.noreply.github.com>
2023-05-08 18:42:40 +0000

    commit-changes-because-code-is-not-working

---
+++ .github/Playground/test.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+print("hello my name is jayme")
+
+# Define the release and runtime integer variables below:
+an_int = 2023
+release_year = an_int
+
+b_int = 1
+runtime = b_int
+
+# Define the rating_out_of_10 float variable below: 
+
+an_float = 9.9
+rating_out_of_10 = an_float
+
+print("Barbie, Dir. Greta Gerwig, was released in") release_year 
+("with a runtime of") runtime ("and earned an audience rating of") rating_out_of_10
